c741c0b897ec7e703380254f302341c20c16bbca
Lewis Holmes <ibeef1990@gmail.com>
2020-05-04 08:30:12 +0100

    Initial commit -Added json-server npm functionality

--- public/js/index.js
+++ public/js/index.js
@@ -47903,7 +47903,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-react_dom__WEBPACK_IMPORTED_MODULE_2___default.a.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_App__WEBPACK_IMPORTED_MODULE_3__["default"], null), document.getElementById('root'));
+react_dom__WEBPACK_IMPORTED_MODULE_2___default.a.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_App__WEBPACK_IMPORTED_MODULE_3__["default"], null), document.getElementById("root"));
 
 /***/ }),
 
@@ -47925,8 +47925,8 @@ react_dom__WEBPACK_IMPORTED_MODULE_2___default.a.render( /*#__PURE__*/react__WEB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/ibeef/Programming/Web_Dev/PHP_Sites/laravel-react-base/resources/js/index.js */"./resources/js/index.js");
-module.exports = __webpack_require__(/*! /Users/ibeef/Programming/Web_Dev/PHP_Sites/laravel-react-base/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /srv/http/it-logger-laravel-react/resources/js/index.js */"./resources/js/index.js");
+module.exports = __webpack_require__(/*! /srv/http/it-logger-laravel-react/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
